dc1e729398c258caef5ca06eb62bdcf6b4fda5e2
SanjarKeySkills <forest2004@mail.ru>
2025-05-09 12:16:03 +0600

    09/05/25| params async Promise productId

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,11 @@
-export default function ProductDetails() {
-    return <h1>Details about products</h1>;
+export default async function ProductDetails({
+    // здесь ставим async (так как вытаскиваем id по каждому товару)
+    params,
+}: // params как аргумент так как он позволяет выбрать по какому id на какой продукт выходить
+{
+    params: Promise<{ productId: string }>;
+    // здесть обращаемся к Promise и деструктуризируем productId в стринговом значении
+}) {
+    const productId = (await params).productId;
+    return <h1>Details about products {productId}</h1>;
 }
