78aef466555ad281eedc6bf7d5bb16840ae112c3
SanjarKeySkills <forest2004@mail.ru>
2025-05-09 16:56:00 +0600

    09/05/25| deleted extra layouts + not-found

--- src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -4,11 +4,13 @@ import { usePathname } from "next/navigation";
 
 export default function NotFound() {
     const pathname = usePathname();
-    const propductId = pathname.split("/")[2];
+    const productId = pathname.split("/")[2];
     const reviewId = pathname.split("/")[4];
     return (
         <div>
-            <h2>Review {reviewId} not found</h2>
+            <h2>
+                Review {reviewId} not found for product {productId}
+            </h2>
         </div>
     );
 }
