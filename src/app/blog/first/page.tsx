c7e606dd5c6580f4a1bb22f9ad68e638705fd297
SanjarKeySkills <forest2004@mail.ru>
2025-05-09 11:43:15 +0600

    09/05/25| blog spot

--- src/app/blog/first/page.tsx
+++ src/app/blog/first/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-    return <h1>First page</h1>;
+    return <h1>First blog spot</h1>;
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-    return <h1>Blog page</h1>;
+    return <h1>Second blog spot</h1>;
 }
