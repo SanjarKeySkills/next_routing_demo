6fafd1b18a855215a65ae798cd288cac16309268
SanjarKeySkills <forest2004@mail.ru>
2025-05-11 14:26:57 +0600

    11/05/25| Metadata

--- src/app/counter/page.tsx
+++ src/app/counter/page.tsx
@@ -1,13 +1,9 @@
-"use client";
+import { Counter } from "./counter";
 
-import { useState } from "react";
+export const metadata = {
+    title: "Counter",
+};
 
 export default function CounterPage() {
-    const [count, setCount] = useState(0);
-    return (
-        <div>
-            <p>Count: {count}</p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-        </div>
-    );
+    return <Counter />;
 }
