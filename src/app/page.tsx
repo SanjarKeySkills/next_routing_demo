c77cf78efb81f8dfb7b13e92d0be06301a560c4e
SanjarKeySkills <forest2004@mail.ru>
2025-05-12 23:43:28 +0600

    12/05/25| lang in french

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,9 @@ export default function Home() {
             <Link href="/articles/breaking-news-123?lang=en">
                 Read in English
             </Link>
+            <Link href="/articles/breaking-news-123?lang=fr">
+                Read in French
+            </Link>
         </div>
     );
 }
