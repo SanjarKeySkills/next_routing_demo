0748a13c46fb6650c25a777e0bd5015b9d25332b
SanjarKeySkills <forest2004@mail.ru>
2025-05-12 23:48:19 +0600

    12/05/25| articles language en

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function NewsArticle() {
     return (
         <div>
             <h1>News article id</h1>
             <p>Reading in language</p>
             <div>
-                <Link></Link>
+                <Link href="/articles/id?lang=en">English</Link>
             </div>
         </div>
     );
