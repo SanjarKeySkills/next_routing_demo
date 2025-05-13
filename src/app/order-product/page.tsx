c7444dc4c735727a08e49ea38b948f5ebb848617
SanjarKeySkills <forest2004@mail.ru>
2025-05-13 12:01:24 +0600

    13/05/25| useRouter

--- src/app/order-product/page.tsx
+++ src/app/order-product/page.tsx
@@ -1,8 +1,10 @@
 import { useRouter } from "next/navigation";
 
 export default function OrderProduct() {
+    const router = useRouter();
     const handleClick = () => {
         console.log("Placing your order");
+        router.push("/");
     };
     return (
         <>
