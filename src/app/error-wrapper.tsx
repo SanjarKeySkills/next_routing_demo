51f27e117aec8f0c3a36b1730a6bd3c9eabe013e
SanjarKeySkills <forest2004@mail.ru>
2025-05-22 00:22:12 +0600

    22/05/25| added error-wrapper tsx

--- src/app/error-wrapper.tsx
+++ src/app/error-wrapper.tsx
@@ -16,5 +16,21 @@ const ErrorSimulator = ({
     const [error, setError] = useState(false);
 
     if (error) throw new Error(message);
-    return <button title="Simulate an error"></button>;
+
+    return (
+        <button title="Simulate an error" onClick={() => setError(true)}>
+            Simulate Error
+        </button>
+    );
+};
+
+export const ErrorWrapper = ({ children }: WrapperProps) => {
+    return (
+        <div className="flex flex-col rounded-lg mt-8 relative p-4 border border-gray-300">
+            <div className="absolute top-0 left-4 -translate-y-1/2">
+                <ErrorSimulator message="Simulated error in root layout" />
+            </div>
+            {children}
+        </div>
+    );
 };
