c9f71924fe9adf5e733d7371bfd677b2c9a3b92f
SanjarKeySkills <forest2004@mail.ru>
2025-05-22 00:37:51 +0600

    22/05/25| itteration to global handling errors

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -1,66 +1,66 @@
-"use client";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useState } from "react";
+// "use client";
+// import Link from "next/link";
+// import { usePathname } from "next/navigation";
+// import { useState } from "react";
 
-const navLinks = [
-    { name: "Register", href: "/register" },
-    { name: "Login", href: "/login" },
-    { name: "Forgot Password", href: "/forgot-password" },
-];
+// const navLinks = [
+//     { name: "Register", href: "/register" },
+//     { name: "Login", href: "/login" },
+//     { name: "Forgot Password", href: "/forgot-password" },
+// ];
 
-export default function AuthLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    const [input, setInput] = useState("");
-    const pathname = usePathname();
-    return (
-        <div>
-            <div>
-                <input
-                    value={input}
-                    onChange={(e) => setInput(e.target.value)}
-                />
-                {/* Здесь сохранен темплейт который не меняется. но в отличие от лэйаута все элементы запускаются 
-				заново кроме темплейта */}
-            </div>
-            {navLinks.map((link) => {
-                const isActive =
-                    pathname === link.href ||
-                    (pathname.startsWith(link.href) && link.href !== "/");
-                return (
-                    <div
-                        style={{
-                            display: "flex",
-                            justifyContent: "flex-start",
-                            flexDirection: "row",
-                        }}>
-                        <div>
-                            <Link href={link.href} key={link.name}>
-                                {link.name}
-                            </Link>
-                        </div>
-                    </div>
-                );
-            })}
+// export default function AuthLayout({
+//     children,
+// }: {
+//     children: React.ReactNode;
+// }) {
+//     const [input, setInput] = useState("");
+//     const pathname = usePathname();
+//     return (
+//         <div>
+//             <div>
+//                 <input
+//                     value={input}
+//                     onChange={(e) => setInput(e.target.value)}
+//                 />
+//                 {/* Здесь сохранен темплейт который не меняется. но в отличие от лэйаута все элементы запускаются
+// 				заново кроме темплейта */}
+//             </div>
+//             {navLinks.map((link) => {
+//                 const isActive =
+//                     pathname === link.href ||
+//                     (pathname.startsWith(link.href) && link.href !== "/");
+//                 return (
+//                     <div
+//                         style={{
+//                             display: "flex",
+//                             justifyContent: "flex-start",
+//                             flexDirection: "row",
+//                         }}>
+//                         <div>
+//                             <Link href={link.href} key={link.name}>
+//                                 {link.name}
+//                             </Link>
+//                         </div>
+//                     </div>
+//                 );
+//             })}
 
-            {children}
-        </div>
-    );
-}
-//  <header
-//   style={{
-//     backgroundColor: "aqua",
-//     padding: "1rem",
-//     }}>
-//     <p>This is header for auth</p>
-//     </header>
-//     <footer
-//     style={{
-//     backgroundColor: "yellowgreen",
-//     padding: "1rem",
-//     }}>
-//     <p>Footer for auth</p>
-// </footer>
+//             {children}
+//         </div>
+//     );
+// }
+// //  <header
+// //   style={{
+// //     backgroundColor: "aqua",
+// //     padding: "1rem",
+// //     }}>
+// //     <p>This is header for auth</p>
+// //     </header>
+// //     <footer
+// //     style={{
+// //     backgroundColor: "yellowgreen",
+// //     padding: "1rem",
+// //     }}>
+// //     <p>Footer for auth</p>
+// // </footer>
