4a71b6e810376c4d651a26dabaa9c6a2556763a4
SanjarKeySkills <forest2004@mail.ru>
2025-05-28 13:50:43 +0600

    28/05/25| added UI and folder UI photo-feed

---
+++ src/app/photo-feed/wonders.ts
@@ -0,0 +1,51 @@
+const wondersImages: WonderImage[] = [
+    {
+        id: "1",
+        name: "Ecology one",
+        src: "./photos/photo1.png",
+        author: "Askat",
+        location: "Mramornoe",
+    },
+    {
+        id: "2",
+        name: "Ecology two",
+        src: "./photos/photo2.png",
+        author: "Askat",
+        location: "Mramornoe",
+    },
+    {
+        id: "3",
+        name: "Ecology three",
+        src: "./photos/photo3.png",
+        author: "Askat",
+        location: "Mramornoe",
+    },
+    {
+        id: "4",
+        name: "Ecology four",
+        src: "./photos/photo4.png",
+        author: "Askat",
+        location: "Mramornoe",
+    },
+    {
+        id: "5",
+        name: "Ecology five",
+        src: "./photos/photo5.png",
+        author: "Askat",
+        location: "Mramornoe",
+    },
+    {
+        id: "6",
+        name: "Ecology six",
+        src: "./photos/photo6.png",
+        author: "Askat",
+        location: "Mramornoe",
+    },
+    {
+        id: "7",
+        name: "Ecology seven",
+        src: "./photos/photo7.png",
+        author: "Askat",
+        location: "Mramornoe",
+    },
+];
