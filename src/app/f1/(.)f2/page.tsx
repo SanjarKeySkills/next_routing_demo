f3bf45652f50fe858fd751a4f7a5976b8801ea14
SanjarKeySkills <forest2004@mail.ru>
2025-05-28 12:09:37 +0600

    28/05/25| added f3 folder

--- src/app/f1/(.)f2/page.tsx
+++ src/app/f1/(.)f2/page.tsx
@@ -1,3 +1,6 @@
 export default function InterceptedF2() {
     return <h1>(.) Intercepted F2 page</h1>;
 }
+
+// (.) здесь как уровень на котором должен отображаться компонент в рамках одного layout
+// можно и (..) nj move this component one level above
