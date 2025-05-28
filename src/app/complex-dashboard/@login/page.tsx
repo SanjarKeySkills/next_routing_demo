6f42a4f2167601b5a2b1445e64c1400ec0fbf562
SanjarKeySkills <forest2004@mail.ru>
2025-05-28 11:28:20 +0600

    28/05/25| finished conditional routes

--- src/app/complex-dashboard/@login/page.tsx
+++ src/app/complex-dashboard/@login/page.tsx
@@ -3,3 +3,4 @@ import { Card } from "@/components/card";
 export default function LoginSlot() {
     return <Card>Please login to continue</Card>;
 }
+// теперь к этой папке можно приписывать отдельно страничку 404 и неверного пароля.
