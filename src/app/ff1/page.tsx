8d973978c61393103e6c3957e3ed5ccebabe4603
SanjarKeySkills <forest2004@mail.ru>
2025-05-30 16:03:26 +0600

    30/05/25| added FF3 segment

--- src/app/ff1/page.tsx
+++ src/app/ff1/page.tsx
@@ -6,6 +6,7 @@ export default function FF1() {
             <h1>FF1 page</h1>
             <div>
                 <Link href={"/ff1/ff2"}>FF2</Link>
+                <Link href={"/ff3"}>FF3</Link>
             </div>
         </>
     );
---
+++ src/app/ff3/page.tsx
@@ -0,0 +1,3 @@
+export default function FF3() {
+    return <h1>FF3 page</h1>;
+}
